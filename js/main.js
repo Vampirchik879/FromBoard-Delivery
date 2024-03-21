cd4916150a9d4fb6c12630b4cf14974f2fee67ac
Vampirchik879 <152918723+Vampirchik879@users.noreply.github.com>
2024-03-21 21:58:07 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){  
+  $("#header_navigation_text1").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#header_navigation_text2").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#header_navigation_text3").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#header_navigation_text4").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+
+function scrollToElement(element) {
+  document.querySelector(element).scrollIntoView({behavior: 'smooth'});
+}
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+  $("#footer_nav_text2").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#footer_nav_text3").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#footer_nav_text4").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function(){  
+  $("#footer_nav_text5").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+  
